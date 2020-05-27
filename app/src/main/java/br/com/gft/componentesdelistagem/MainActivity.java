06cd263cd870b0f115e57ccb7cbd4ed18bfa9ceb
leandro-gft <lpsacchi@gmail.com>
2020-05-27 12:20:52 -0300

    Adicionada ListView

--- app/src/main/java/br/com/gft/componentesdelistagem/MainActivity.java
+++ app/src/main/java/br/com/gft/componentesdelistagem/MainActivity.java
@@ -2,13 +2,57 @@ package br.com.gft.componentesdelistagem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listItens;
+    private String[] itens = {
+            "Campinas", "Sumaré", "São Paulo", "Cajamar", "Barueri", "Limeira", "Americana",
+            "Valinhos", "Vinhedo", "Louveira", "Jundiaí", "Santana do Parnaíba", "Carapicuíba",
+            "Osasco"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listItens = findViewById(R.id.listItens);
+
+        //Cria adaptador para a lista
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        //Adiciona adaptador à lista
+        listItens.setAdapter(adapter);
+
+        //Adiciona clique na lista
+        listItens.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Item selecionado: "+listItens.getItemAtPosition(position),
+                        Toast.LENGTH_SHORT).
+                        show();
+            }
+        });
+
+
+    }
+
+    public void nextRecyclerView(View view){
+     //   startActivity(new Intent(this, RecyclerView.class));
     }
 }
---
+++ app/src/main/java/br/com/gft/componentesdelistagem/RecyclerViewList.java
@@ -0,0 +1,14 @@
+package br.com.gft.componentesdelistagem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecyclerViewList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_list);
+    }
+}
