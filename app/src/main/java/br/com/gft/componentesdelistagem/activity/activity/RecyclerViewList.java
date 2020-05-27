dcb2a7d4804ef9a3e16b275b1e601a29b6c6703a
leandro-gft <lpsacchi@gmail.com>
2020-05-27 13:56:34 -0300

    Adcionados lista de filmes da RecyclerView

--- app/src/main/java/br/com/gft/componentesdelistagem/activity/activity/RecyclerViewList.java
+++ app/src/main/java/br/com/gft/componentesdelistagem/activity/activity/RecyclerViewList.java
@@ -1,17 +1,25 @@
 package br.com.gft.componentesdelistagem.activity.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import br.com.gft.componentesdelistagem.R;
 import br.com.gft.componentesdelistagem.activity.adapter.AdapterFilmes;
+import br.com.gft.componentesdelistagem.activity.model.Filme;
 
 public class RecyclerViewList extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private List<Filme> filmes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +28,11 @@ public class RecyclerViewList extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
+        //Listagem de Filmes
+        this.criarFilmes();
+
         //Configurar Adapter
-        AdapterFilmes adapter = new AdapterFilmes();
+        AdapterFilmes adapter = new AdapterFilmes(filmes);
 
         //Configurar RecyclerView
 
@@ -29,5 +40,28 @@ public class RecyclerViewList extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+    }
+
+
+    public void criarFilmes(){
+        Filme f1 = new Filme("Parasita", "2019", "Drama");
+        Filme f2 = new Filme("1917", "2019", "Drama");
+        Filme f3 = new Filme("Ford vs Ferrari", "2019", "Drama");
+        Filme f4 = new Filme("Coringa", "2019", "Drama");
+        Filme f5 = new Filme("Era Uma Vez em Hollywood", "2019", "Drama");
+        Filme f6 = new Filme("O Irlandês", "2019", "Drama");
+        Filme f7 = new Filme("Adoráveis Mulheres", "2019", "Drama");
+        Filme f8 = new Filme("Jojo Rabbit", "2019", "Drama");
+        Filme f9 = new Filme("História de um Casamento", "2019", "Drama");
+        Filme f10 = new Filme("Dor e Glória", "2019", "Drama");
+        Filme f11 = new Filme("Dois Papas", "2019", "Drama");
+        Filme f12 = new Filme("Harriet", "2019", "Drama");
+        Filme f13 = new Filme("Toy Story 4", "2019", "Animação");
+        Filme f14 = new Filme("Klaus", "2019", "Animação");
+        Filme f15 = new Filme("Os Vingadores: O Ultimato", "2019", "Aventura");
+
+        filmes.addAll(Arrays.asList(f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15));
+
     }
 }
--- app/src/main/java/br/com/gft/componentesdelistagem/activity/adapter/AdapterFilmes.java
+++ app/src/main/java/br/com/gft/componentesdelistagem/activity/adapter/AdapterFilmes.java
@@ -10,9 +10,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import org.w3c.dom.Text;
 
+import java.util.List;
+
 import br.com.gft.componentesdelistagem.R;
+import br.com.gft.componentesdelistagem.activity.model.Filme;
 
 public class AdapterFilmes extends RecyclerView.Adapter<AdapterFilmes.MyViewHolder> {
+
+    private List<Filme> listaFilmes;
+
+    public AdapterFilmes(List<Filme> lista) {
+        this.listaFilmes = lista;
+    }
 
     @NonNull
     @Override
@@ -25,14 +34,15 @@ public class AdapterFilmes extends RecyclerView.Adapter<AdapterFilmes.MyViewHold
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.titulo.setText("Parasita");
-        holder.ano.setText("2019");
-        holder.genero.setText("Drama");
+        Filme filme = listaFilmes.get(position);
+        holder.titulo.setText(filme.getTituloFilme());
+        holder.ano.setText(filme.getAnoFilme());
+        holder.genero.setText(filme.getGeneroFilme());
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return listaFilmes.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
