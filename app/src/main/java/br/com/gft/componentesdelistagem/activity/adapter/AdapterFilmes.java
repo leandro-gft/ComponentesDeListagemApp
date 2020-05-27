9a8c6b8e467fd6bfcac19c9cca0b627d4369f87c
leandro-gft <lpsacchi@gmail.com>
2020-05-27 13:05:39 -0300

    Configura MyViewHolder

--- app/src/main/java/br/com/gft/componentesdelistagem/activity/adapter/AdapterFilmes.java
+++ app/src/main/java/br/com/gft/componentesdelistagem/activity/adapter/AdapterFilmes.java
@@ -1,5 +1,6 @@
 package br.com.gft.componentesdelistagem.activity.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,23 +10,29 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import org.w3c.dom.Text;
 
+import br.com.gft.componentesdelistagem.R;
+
 public class AdapterFilmes extends RecyclerView.Adapter<AdapterFilmes.MyViewHolder> {
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        return null;
+        //Converter xml em objeto view
+        View itemLista = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.adapter_lista, parent, false);
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.titulo.setText("Parasita");
+        holder.ano.setText("2019");
+        holder.genero.setText("Drama");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 5;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -36,6 +43,11 @@ public class AdapterFilmes extends RecyclerView.Adapter<AdapterFilmes.MyViewHold
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            titulo = itemView.findViewById(R.id.textTitulo);
+            ano = itemView.findViewById(R.id.textAno);
+            genero = itemView.findViewById(R.id.textGenero);
+
         }
     }
 
